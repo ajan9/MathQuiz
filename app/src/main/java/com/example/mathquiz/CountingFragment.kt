2eff906db69c55382580f41a5a777eda5e236738
Anđela Jaković <andela.jakovic@asseco-see.hr>
2023-02-27 16:48:53 +0100

    Fixing layouts

--- app/src/main/java/com/example/mathquiz/CountingFragment.kt
+++ app/src/main/java/com/example/mathquiz/CountingFragment.kt
@@ -1,13 +1,15 @@
 package com.example.mathquiz
 
-import android.content.res.Resources
+import android.app.ActionBar
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.example.mathquiz.databinding.FragmentCountingBinding
 import com.example.mathquiz.model.Fruit
 
@@ -28,6 +30,7 @@ class CountingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+//        binding.tableLayout.layoutParams = ViewGroup.LayoutParams(binding.tableLayout.width, (binding.tableLayout.width * 2) / 5)
         newGame()
     }
 
@@ -56,8 +59,8 @@ class CountingFragment : Fragment() {
         fruits.addAll(
             listOf(
                 Fruit(R.string.banana, R.drawable.banana),
+//                Fruit(R.string.cherry, R.drawable.cherry),
                 Fruit(R.string.strawberry, R.drawable.strawberry),
-                Fruit(R.string.cherry, R.drawable.cherry),
                 Fruit(R.string.lemon, R.drawable.lemon),
                 Fruit(R.string.peach, R.drawable.peach)
             )
@@ -91,19 +94,58 @@ class CountingFragment : Fragment() {
                 binding.buttonTen
             )
         )
-        for (button in buttons) {
-            button.isClickable = true
-            button.background = resources.getDrawable(R.drawable.blue_button_background)
 
+        buttons.forEach {
+            it.isClickable = true
+        }
+
+        for (button in buttons) {
             button.setOnClickListener {
+                buttons.forEach { it.isClickable = false }
+
                 if (button.text == number.toString()) {
-                    newGame()
+                    button.background = resources.getDrawable(R.drawable.green_button_backgorund)
+                    Handler(Looper.getMainLooper()).postDelayed(
+                        {
+                            button.background = resources.getDrawable(R.drawable.gray_button_background)
+                            newGame()
+                        },
+                        700 // value in milliseconds
+                    )
                 } else {
+                    lateinit var tmpButton: Button
                     button.background = resources.getDrawable(R.drawable.red_button)
-                    button.isClickable = false
+                    buttons.forEach {
+                        if (it.text == number.toString()) {
+                            it.background = resources.getDrawable(R.drawable.green_button_backgorund)
+                            tmpButton = it
+                        }
+                    }
+                    Handler(Looper.getMainLooper()).postDelayed(
+                        {
+                            button.background = resources.getDrawable(R.drawable.gray_button_background)
+                            tmpButton.background = resources.getDrawable(R.drawable.gray_button_background)
+                            newGame()
+                        },
+                        1000 // value in milliseconds
+                    )
                 }
             }
         }
+//
+//        for (button in buttons) {
+//            button.isClickable = true
+//            button.background = resources.getDrawable(R.drawable.blue_button_background)
+//
+//            button.setOnClickListener {
+//                if (button.text == number.toString()) {
+//                    newGame()
+//                } else {
+//                    button.background = resources.getDrawable(R.drawable.red_button)
+//                    button.isClickable = false
+//                }
+//            }
+//        }
     }
 
     override fun onDestroyView() {
